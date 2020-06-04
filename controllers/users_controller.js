dd523b7428b643a33c0376a81d5c274253ce682b
samakshimalhotra <samakshimalhotra24@gmail.com>
2020-06-04 20:50:48 +0530

    users router is added to link up with normal router

---
+++ controllers/users_controller.js
@@ -0,0 +1,8 @@
+//this is going to controll many users
+module.exports.profile=function(req,res){
+    res.end('<h1>user Profile</h1>');
+}
+
+module.exports.post=function(req,res){
+    res.end('<h1>user Post</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-const express=require('express');
+const express= require('express');
 
 const router=express.Router();
 const homeController= require('../controllers/home_controller');
 
 console.log('router loaded');
+
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+//for any further routes,acess from here
+//router.use('/routerName',require('./routerfile'));
 module.exports=router;
