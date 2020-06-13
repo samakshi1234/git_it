4456378e8d2e032f80149085a56566dd81abb569
samakshimalhotra <samakshimalhotra24@gmail.com>
2020-06-13 15:26:17 +0530

    setup the schema for posts,saved the post in db and showed it on home view

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,12 @@
-module.exports.home=function(req,res){
-    return res.render('home',{
-    title:"Home"
-    });
+const Post=require('../models/post');
 
+module.exports.home=function(req,res){
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+            });
+    });
+   
 
 }
