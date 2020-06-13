0cd8b9de7c1c73b47ec4fbeb418931952726b4a1
samakshimalhotra <samakshimalhotra24@gmail.com>
2020-06-14 02:36:27 +0530

    converted to async await format and scss is used to  put friend links alogside posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,29 @@
 const Post=require('../models/post');
 const User=require('../models/users');
-module.exports.home=function(req,res){
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        
-        User.find({},function(err,users){
-            return res.render('home',{
-                title:"Codeial | Home",
-                posts:posts,
-                all_users:users
-                });
-        });
-    });
-   
+module.exports.home= async function(req,res){
 
+  try
+   {
+        let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+
+        let users=await User.find({});
+
+        return res.render('home',{
+                    title:"Codeial | Home",
+                    posts:posts,
+                    all_users:users
+        });
+
+    }
+    catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
