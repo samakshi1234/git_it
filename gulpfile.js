225f67405e9c7320c9ed7712535b79df1283efab
samakshimalhotra <samakshimalhotra24@gmail.com>
2020-07-02 18:32:55 +0530

    changing localhost port to 2000

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,71 @@
-const gulp= require('gulp');
+const gulp = require('gulp');
 
-const sass= require('gulp-sass');
-const cssnano= require('gulp-cssnano');
-
-const rev=require('gulp-rev');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 
 
-gulp.task('css',function(done){
-    console.log('minifying css...');
-    gulp.src('./assests/sass/**/*.scss')
+gulp.task('css', function(done){
+    console.log('css minifying');
+    gulp.src('./assests/scss/**/*.scss')
     .pipe(sass())
     .pipe(cssnano())
-    .pipe(gulp.dest('./assests.css'))
-
-   gulp.src('./assests/**/*.css')
+    .pipe(gulp.dest('./assets/css'));
+   
+    gulp.src('./assests/**/*.css')
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd:'public',
-        merge:true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
-})
+       .pipe(gulp.dest('./public/assets'))  // write rev'd assets to build dir
+       .pipe(rev.manifest({
+           base:'./public/assets',
+           merge:true
+       }))
+       .pipe(gulp.dest('./public/assets'));
+       done();
+   
+   })
+   
+   gulp.task('js', function(done){
+       console.log('minifying js...');
+        gulp.src('./assests/**/*.js')
+       .pipe(uglify())
+       .pipe(rev())
+       .pipe(gulp.dest('./public/assets'))  // write rev'd assets to build dir
+       .pipe(rev.manifest({
+           base:'./public/assets',
+           merge:true
+       }))
+       .pipe(gulp.dest('./public/assets'))
+       done();
+   });
 
-gulp.task('js', function(done){
-    console.log('minifying js...');
-     gulp.src('./assests/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
-});
-
-
-gulp.task('images', function(done){
+   gulp.task('images', function(done){
     console.log('compressing images...');
     gulp.src('./assests/**/*.+(png|jpg|gif|svg|jpeg)')
     .pipe(imagemin())
+    // .pipe(gulp.dest('./public/assets'))
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
+        .pipe(gulp.dest('./public/assets'))  // write rev'd assets to build dir
+        .pipe(rev.manifest({
+            base:'./public/assets',
+            merge:true
+        }))
+        .pipe(gulp.dest('./public/assets'));
+        done();
 });
-
-
-// empty the public/assets directory
-gulp.task('clean:assets', function(done){
-    del.sync('./public/assets');
-    done();
-});
-
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
-    console.log('Building assets');
-    done();
-});
+   
+   
+   gulp.task('clean:assets', function(done){
+       del.sync('./public');
+       del.sync('./rev-manifest.json');
+       //this will delete the manifest every time gulp build runs ,so may solve your problem of manifest adding,deleting,etc.
+       done();
+   });
+   
+   
+   gulp.task('build', gulp.series('clean:assets' , 'images' , 'css' , 'js'), function(done){
+       console.log('Building assets');
+       done();
+   });
